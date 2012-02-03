efed266b8e064f896caac3bbb4a1236b875d7d10
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-02-03 02:31:19 +0100

    Documenting plus various nitpicks.

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
+'''
+Global variables.
+Some (especially pathes) are more likely to be changed than others.
+'''
 TAGS_PATTERN = '{{ (\S+) }}'
-LILYPOND_VERSION_PATTERN = 'GNU LilyPond ([0-9\.]+)'
 LILYPOND_PATH = ''
 LILYPOND_COMMAND = LILYPOND_PATH + 'lilypond'
+LILYPOND_VERSION_PATTERN = 'GNU LilyPond ([0-9\.]+)'
 
--- titelouze.py
+++ titelouze.py
@@ -1,19 +1,45 @@
-import subprocess, re
+# coding: utf-8
+'''
+Titelouze
+Bertrand Bordage © 2011
+
+This application is a framework for LilyPond, a music engraving program.
+Its goal is to provide an easy way to create large music books.
+'''
+
+import subprocess, sys, re, os
 from settings import *
 
 class LilyPond:
     def launch(self, *args, **kwargs):
+        '''
+        Launches LilyPond with raw args and kwargs passed as "--key=value".
+        Returns the output.
+        launch([...], verbose=True) will flush the output to stdout.
+        '''
+        verbose = kwargs.get('verbose', False)
+        if 'verbose' in kwargs:
+            del kwargs['verbose']
         command = [LILYPOND_COMMAND]
         for arg in args:
-            command.append('--'+arg)
+            command.append(arg)
         for key in kwargs:
             command.append('--'+key+'='+kwargs[key])
         p = subprocess.Popen(args=command, stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT, shell=False)
+        lines = ''
+        for line in iter(p.stdout.readline, ""):
+            if verbose:
+                sys.stdout.write(line)
+                sys.stdout.flush()
+            lines += line
         p.wait()
-        return ''.join(p.stdout.readlines())
+        return lines
     def get_version(self):
-        lines = self.launch('version')
+        '''
+        Returns LilyPond version number.
+        '''
+        lines = self.launch('-v')
         try:
             return re.findall(LILYPOND_VERSION_PATTERN, lines)[0]
         except:
@@ -22,7 +48,10 @@ class LilyPond:
 class Titelouze:
     def __init__(self):
         self.tags_object = re.compile(TAGS_PATTERN)
-    def replace(self, filename):
+    def replace_tags(self, filename):
+        '''
+        Replaces template tags with the corresponding values.
+        '''
         f = open(filename, 'r')
         lines = ''.join(f.readlines())
         f.close()
@@ -38,12 +67,21 @@ class Titelouze:
                 out += tag
         return out
     def output(self, filename, lines):
+        '''
+        Saves "lines" in the file named "filename".
+        '''
+        dir = os.path.dirname(filename)
+        if dir and not os.path.exists(dir):
+            os.makedirs(dir)
         f = open(filename, 'w')
         f.writelines(lines)
         f.close()
 
 if __name__ == '__main__':
-    version = LilyPond().get_version()
-    lines = Titelouze().replace('templates/base.ily')
-    Titelouze().output('out.ly', lines)
+    lilypond = LilyPond()
+    titelouze = Titelouze()
+    version = lilypond.get_version()
+    lines = titelouze.replace_tags('templates/base.ily')
+    titelouze.output('out.ly', lines)
+    lilypond.launch('out.ly', verbose=True)
 
